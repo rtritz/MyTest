4b4e9861fb71827696fb2ed81544cfe9265e100c
rritz@parktudor.org <Hopsey12345$>
2021-10-14 10:03:31 -0400

    Add Driver2

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,26 @@
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello World"); // print hello world
+        for(int i = 101; i <= 999; i+=2) {
+            String s = "" + i;
+            int n1 = Integer.parseInt(s.substring(0, 1));
+            int n2 = Integer.parseInt(s.substring(1, 2));
+            int n3 = Integer.parseInt(s.substring(2, 3));
+
+            int sum = n1 + n2 + n3;
+
+            if (sum == 15) {
+                //System.out.println("sum: " + i);
+                if(n1 % 2 != 0 && n2 % 2 != 0 && n3 % 2 != 0) {
+                    //System.out.println("odd digits: " + i);
+                    if(n1 < n2 && n2 < n3) {
+                        System.out.println("ascending: " + i);
+                        if(i % 7 == 0)
+                            System.out.println(i);
+                    }
+                }
+            }
+
+        }
 
     }
 }
--- src/Driver2.java
+++ src/Driver2.java
@@ -1,5 +1,12 @@
 public class Driver2 {
     public static void main(String[] args) {
         System.out.println("Good bye");
+
+        int sum = 0;
+
+        for(int i = 10; i <= 20; i++)
+            sum += i;
+
+        System.out.println(sum);
     }
 }
